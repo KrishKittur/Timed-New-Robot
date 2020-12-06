f037799890192de5edf634437c7bae6cea13e04a
KrishKittur <kkittur24@dtechhs.org>
2020-12-05 17:02:08 -0800

    Inital Commit

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+
+public class Ports {
+    static int MOTOR_ONE_ID = 20;
+    static int MOTOR_TWO_ID = 21;
+    static int CONTROLLER_CHANNEL = 0;
+}
---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+
+    CANSparkMax motorOne = new CANSparkMax(Ports.MOTOR_ONE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    CANSparkMax motorTwo = new CANSparkMax(Ports.MOTOR_TWO_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+    XboxController controller = new XboxController(Ports.CONTROLLER_CHANNEL);
+
+    @Override
+    public void teleopPeriodic() {
+
+      if (controller.getY(Hand.kLeft) > 0.1 || controller.getY(Hand.kLeft) < -0.1) {
+        motorOne.set(controller.getY(Hand.kLeft));
+      }
+
+      if (controller.getY(Hand.kRight) > 0.1 || controller.getY(Hand.kRight) < -0.1) {
+        motorTwo.set(controller.getY(Hand.kRight));
+      }
+
+    }
+
+}
