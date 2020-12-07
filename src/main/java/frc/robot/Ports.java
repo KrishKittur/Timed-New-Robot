1019d58269f931fa628e5c01c7df22160919b127
KrishKittur <kkittur24@dtechhs.org>
2020-12-06 16:56:52 -0800

    Added some stuff

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,5 +4,7 @@ package frc.robot;
 public class Ports {
     static int MOTOR_ONE_ID = 20;
     static int MOTOR_TWO_ID = 21;
+    static int MOTOR_THREE_ID = 22;
+    static int MOTOR_FOUR_ID = 23;
     static int CONTROLLER_CHANNEL = 0;
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import com.revrobotics.CANSparkMaxLowLevel;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,6 +25,8 @@ public class Robot extends TimedRobot {
 
       CANSparkMax motorOne = new CANSparkMax(Ports.MOTOR_ONE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
       CANSparkMax motorTwo = new CANSparkMax(Ports.MOTOR_TWO_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+      CANSparkMax motorThree = new CANSparkMax(Ports.MOTOR_THREE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+      CANSparkMax motorFour = new CANSparkMax(Ports.MOTOR_FOUR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
       XboxController controller = new XboxController(Ports.CONTROLLER_CHANNEL);
 
       @Override
@@ -34,20 +37,28 @@ public class Robot extends TimedRobot {
 
       @Override
       public void teleopPeriodic() {
-
+        
         if (controller.getAButton()) {
-          motorOne.set(1);
+          motorOne.set(0.3);
         } else {
-          motorOne.set(0);
+          motorOne.set(0.00);
         }
 
-        if (controller.getBButton()) {
-          motorTwo.set(1);
+        if (controller.getTriggerAxis(Hand.kLeft) > 0.1) {
+          motorTwo.set(-1.00);
+          motorThree.set(-1 * controller.getTriggerAxis(Hand.kLeft));
+          motorFour.set(controller.getTriggerAxis(Hand.kLeft));
         } else {
-          motorOne.set(0);
+          motorTwo.set(0);
+          motorThree.set(0);
+          motorFour.set(0);
         }
+
+        
 
       }
 
 }
+
+
 
