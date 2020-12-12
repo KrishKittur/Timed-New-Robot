d822061bab4af844adb7d4a6d5949fc98731c938
KrishKittur <kkittur24@dtechhs.org>
2020-12-11 17:03:43 -0800

    Added Encoder Test

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -4,20 +4,11 @@ package frc.robot;
 public class Ports {
 
     // Ports
-    static int FLYWHEEL_ENCODER_CHANNEL_A = 1;
-    static int FLYWHEEL_ENCODER_CHANNEL_B = 2;
-    static int MOTOR_ONE_ID = 20;
-    static int MOTOR_TWO_ID = 21;
-    static int MOTOR_THREE_ID = 22;
-    static int MOTOR_FOUR_ID = 23;
+    static int FLYWHEEL_ENCODER_CHANNEL_A = 0;
+    static int FLYWHEEL_ENCODER_CHANNEL_B = 1;
+    static int INTAKE_MOTOR_ID = 20;
+    static int SUCKER_MOTOR_ID = 21;
+    static int FLYWHEEL_MOTOR_1_ID = 22;
+    static int FLYWHEEL_MOTOR_2_ID = 23;
     static int CONTROLLER_CHANNEL = 0;
-
-    // For tuning
-    static double Kp = 0;
-    static double Ki = 0;
-    static double Kd = 0;
-    static double setPoint = 4000;
-
-    static double voltagePerRPM = 0;
-    static double staticBreakVoltage = 0;
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2020 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import com.revrobotics.CANSparkMax;
@@ -14,63 +7,53 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpiutil.math.MathUtil;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
 
-      CANSparkMax motorOne = new CANSparkMax(Ports.MOTOR_ONE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-      CANSparkMax motorTwo = new CANSparkMax(Ports.MOTOR_TWO_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-      CANSparkMax motorThree = new CANSparkMax(Ports.MOTOR_THREE_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-      CANSparkMax motorFour = new CANSparkMax(Ports.MOTOR_FOUR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+      // Instantiate all of the hardware
+      CANSparkMax intakeMotor = new CANSparkMax(Ports.INTAKE_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+      CANSparkMax suckerMotor = new CANSparkMax(Ports.SUCKER_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+      CANSparkMax flywheelMotor1 = new CANSparkMax(Ports.FLYWHEEL_MOTOR_1_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
+      CANSparkMax flywheelMotor2 = new CANSparkMax(Ports.FLYWHEEL_MOTOR_2_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
       XboxController controller = new XboxController(Ports.CONTROLLER_CHANNEL);
-      PIDController pid = new PIDController(Ports.Kp, Ports.Ki, Ports.Kd);
-      SimpleMotorFeedforward ff = new SimpleMotorFeedforward(Ports.staticBreakVoltage, Ports.voltagePerRPM);
       Encoder flyWheelEncoder = new Encoder(Ports.FLYWHEEL_ENCODER_CHANNEL_A, Ports.FLYWHEEL_ENCODER_CHANNEL_B);
       
       double velocity;
 
       @Override
       public void teleopInit() {
-        motorOne.setSmartCurrentLimit(4);
-        motorOne.setSecondaryCurrentLimit(5);
-        pid.setTolerance(10);
-
-        flyWheelEncoder.setDistancePerPulse(Math.PI / 8192);
+        // set setters such as current limits and distances
+        intakeMotor.setSmartCurrentLimit(8);
+        intakeMotor.setSecondaryCurrentLimit(10);
+        flyWheelEncoder.setDistancePerPulse(Math.PI * 2 / 8192);
         flyWheelEncoder.setMinRate(10);
-      
       }
 
       @Override
       public void teleopPeriodic() {
         
+        // If the A button is pressed turn on the intake
         if (controller.getAButton()) {
-          motorOne.set(0.3);
+          intakeMotor.set(0.6);
         } else {
-          motorOne.set(0.00);
+          intakeMotor.set(0);
         }
 
-        if (controller.getBButton()) {
-
-          motorTwo.set(-1.00);
-          motorThree.setVoltage(-1 * MathUtil.clamp(pid.calculate(flyWheelEncoder.getDistance(), Ports.setPoint), -12, 12));
-          motorFour.setVoltage(MathUtil.clamp((pid.calculate(flyWheelEncoder.getDistance(), Ports.setPoint) + ff.calculate(Ports.setPoint)), -12, 12));
-
+        // If the left trigger is pressed turn on the flywheel and sucker
+        if (controller.getTriggerAxis(Hand.kLeft) > 0.1) {
+          suckerMotor.set(-1);
+          flywheelMotor1.set(-1 * controller.getTriggerAxis(Hand.kLeft));
+          flywheelMotor2.set(controller.getTriggerAxis(Hand.kLeft));      
         } else {
-          motorTwo.set(0);
-          motorThree.setVoltage(0);
-          motorFour.setVoltage(0);
-        }     
-        
-      }
+          suckerMotor.set(0);
+          flywheelMotor1.set(0);
+          flywheelMotor2.set(0);
+        }
+
+        System.out.println(flyWheelEncoder.getDistance()); // Print out the flywheel encoders value 
+
+   }
 
 }
 
