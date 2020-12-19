667a45767ccba40ab38fa6989b82e442c7c50309
KrishKittur <kkittur24@dtechhs.org>
2020-12-19 15:52:00 -0800

    .

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,10 +4,8 @@ package frc.robot;
 public class Constants {
 
     // Ports
-    static int FLYWHEEL_ENCODER_CHANNEL_A = 1;
-    static int FLYWHEEL_ENCODER_CHANNEL_B = 0;
-    static int SPINDEXER_MOTOR_ID = 20;
-    static int ACCELERATOR_MOTOR_ID = 21;
+    static int FLYWHEEL_ENCODER_CHANNEL_A = 0;
+    static int FLYWHEEL_ENCODER_CHANNEL_B = 1;
     static int FLYWHEEL_MOTOR_1_ID = 22;
     static int FLYWHEEL_MOTOR_2_ID = 23;
     static int CONTROLLER_CHANNEL = 0;
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,8 +13,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends TimedRobot {
 
       // Instantiate all of the hardware
-      CANSparkMax spindexerMotor = new CANSparkMax(Constants.SPINDEXER_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
-      CANSparkMax acceleratorMotor = new CANSparkMax(Constants.ACCELERATOR_MOTOR_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
       CANSparkMax flywheelMotor1 = new CANSparkMax(Constants.FLYWHEEL_MOTOR_1_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
       CANSparkMax flywheelMotor2 = new CANSparkMax(Constants.FLYWHEEL_MOTOR_2_ID, CANSparkMaxLowLevel.MotorType.kBrushless);
       XboxController controller = new XboxController(Constants.CONTROLLER_CHANNEL);
@@ -23,27 +21,21 @@ public class Robot extends TimedRobot {
       
       @Override
       public void teleopInit() {
-        // Set setters such as current limits and distances
-        spindexerMotor.setSmartCurrentLimit(14);
-        spindexerMotor.setSecondaryCurrentLimit(16);
-
+        flyWheelEncoder.setReverseDirection(true);
+        flyWheelEncoder.setDistancePerPulse(1/8192);
       }
 
       @Override
       public void teleopPeriodic() {  
-      
-        // If the A button is held turn on the intake
-        spindexerMotor.set(controller.getAButton() ? 0.6 : 0);
 
         // If the left trigger is held turn on the flywheel and sucker
         double leftTriggerVal = controller.getTriggerAxis(Hand.kLeft);
 
         flywheelMotor1.set(leftTriggerVal > 0.1 ? -1 * leftTriggerVal : 0);
         flywheelMotor2.set(leftTriggerVal > 0.1 ? leftTriggerVal : 0);
-        acceleratorMotor.set(leftTriggerVal > 0.1 ? -1 : 0);
 
         // Add the flywheels speed to the smart dashboard
-        SmartDashboard.putNumber("Flywheel Distance", flyWheelEncoder.getRate() * 60);
+        SmartDashboard.putNumber("Flywheel Speed", flyWheelEncoder.getRate() * 60);
    }
 
 }
